0888dbcacadce64fc58df2665c3e3e7fdb2a35e5
MalakPL <szmajdakarol@Gmail.com>
2021-10-08 00:08:44 +0200

    Build Fix
    s24574

--- src/s01-div.cpp
+++ src/s01-div.cpp
@@ -2,6 +2,11 @@
 #include <string>
 
 auto main(int argc, char* argv[]) -> int {
+    if (argc != 2) 
+    {
+        return 1;
+    }
+    
     auto const a = std::stoi(argv[1]);
     auto const b = std::stoi(argv[2]);
     std::cout << (a / b) << "\n";
